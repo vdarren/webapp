638e28100483ee7c9be84fc93e9fd3f6827cf071
1303043 <darren.williamson95@yahoo.co.uk>
2016-10-17 14:28:25 +0100

    w1`

---
+++ Week4/formResponse.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+
+    </body>
+    <head>
+
+        <form action="formResponse.php" method="post">
+
+        <label>Forename</label><input type=“text” name=“forename”>
+        <label>Surname</label><input type=“text” name=“surname”>
+        <input type="submit" value="Submit">
+        </form>
+
+    </head>
+
+</html>
---
+++ Week4/superHero.php
@@ -0,0 +1,22 @@
+<html>
+    <body>
+    </body>
+
+
+
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: 1303043
+     * Date: 17/10/2016
+     * Time: 14:26
+     */
+    $forename = $_POST["forename"];
+    $surename = $_POST["surname"];
+    echo $forename . $surename;
+
+
+    ?>
+
+
+</html>
