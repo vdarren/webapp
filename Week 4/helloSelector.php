c0e14d16a7b804ca3b6fe87bf753ec9fd3338dc8
1303043 <darren.williamson95@yahoo.co.uk>
2016-10-17 14:02:30 +0100

    Mate

--- Week 4/helloSelector.php
+++ Week 4/helloSelector.php
@@ -7,7 +7,9 @@
          * Date: 17/10/2016
          * Time: 13:37
          */
-
+        
+        Where would you like to say hello?
+        
         <a href="mypage.php?location=earth"> Earth</a>
         <a href="mypage.php?location=mars"> Mars</a>
         <a href="mypage.php?location=uranus"> Uranus</a>
