e17ed3a1462ba1826018b273ad71fa4198aa9068
1303043 <darren.williamson95@yahoo.co.uk>
2016-10-17 13:54:44 +0100

    Mate

---
+++ Week 4/helloPrinter.php
@@ -0,0 +1,24 @@
+<html>
+    <body>
+
+
+
+        <?php
+/**
+ * Created by PhpStorm.
+ * User: 1303043
+ * Date: 17/10/2016
+ * Time: 13:38
+ */
+
+        echo "Where would you like to say hello?";
+
+        $location = "mypage.php?planet=Earth";
+        header($location);
+        
+        ?>
+
+
+    </body>
+
+</html>
---
+++ Week 4/helloSelector.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+
+        /**
+         * Created by PhpStorm.
+         * User: 1303043
+         * Date: 17/10/2016
+         * Time: 13:37
+         */
+
+        <a href="mypage.php?location=earth"> Earth</a>
+        <a href="mypage.php?location=mars"> Mars</a>
+        <a href="mypage.php?location=uranus"> Uranus</a>
+
+
+
+    </body>
+
+</html>
